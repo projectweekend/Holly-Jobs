fd3c8c4bdccae2d4f3f761f26a95b387df1d6985
Brian Hines <brian@projectweekend.net>
2014-12-12 18:23:07 -0600

    Better

--- SensorReading.js
+++ SensorReading.js
@@ -6,61 +6,60 @@ var connections = require( "./lib/connections" );
 var models = require( "./lib/models" );
 
 
-var logger = connections.logger( config.sensorReadingLogglyTag );
-var db = connections.database();
-var broker = connections.jackrabbit();
-
-
 function main () {
+    var logger = connections.logger( config.sensorReadingLogglyTag );
+    var db = connections.database();
+    var broker = connections.jackrabbit();
+
     broker.once( "connected", createQueue );
-}
 
 
-function createQueue () {
-    broker.create( config.sensorQueue, run );
-}
+    function createQueue () {
+        broker.create( config.sensorQueue, run );
+    }
 
 
-function run () {
-    var previousMinute;
-    setInterval( function () {
-        var currentMinute = moment().minutes();
-        if ( previousMinute !== currentMinute && currentMinute % config.sensorReadingMinute === 0 ) {
-            previousMinute = currentMinute;
-            processSensorReading();
-        }
-    }, config.runInterval );
-}
+    function run () {
+        var previousMinute;
+        setInterval( function () {
+            var currentMinute = moment().minutes();
+            if ( previousMinute !== currentMinute && currentMinute % config.sensorReadingMinute === 0 ) {
+                previousMinute = currentMinute;
+                processSensorReading();
+            }
+        }, config.runInterval );
+    }
 
 
-function processSensorReading () {
-    async.waterfall( [ readSensor, saveData ], function ( err, result ) {
-        if ( err ) {
-            logger.log( err );
-            process.exit( 1 );
-        }
-        logger.log( result );
-    } );
-}
+    function processSensorReading () {
+        async.waterfall( [ readSensor, saveData ], function ( err, result ) {
+            if ( err ) {
+                logger.log( err );
+                process.exit( 1 );
+            }
+            logger.log( result );
+        } );
+    }
 
 
-function readSensor ( done ) {
-    broker.publish( config.sensorQueue, { serialMessage: "A" }, function ( err, data ) {
-        if ( err ) {
-            return done( err );
-        }
-        return done( null, data );
-    } );
-}
+    function readSensor ( done ) {
+        broker.publish( config.sensorQueue, { serialMessage: "A" }, function ( err, data ) {
+            if ( err ) {
+                return done( err );
+            }
+            return done( null, data );
+        } );
+    }
 
 
-function saveData ( sensorData, done ) {
-    models.SensorReading.add( sensorData, function ( err, data ) {
-        if ( err ) {
-            return done( err );
-        }
-        return done( null, data );
-    } );
+    function saveData ( sensorData, done ) {
+        models.SensorReading.add( sensorData, function ( err, data ) {
+            if ( err ) {
+                return done( err );
+            }
+            return done( null, data );
+        } );
+    }
 }
 
 
